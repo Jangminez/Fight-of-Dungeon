dfc8e895a82aa3ecc4d5c4a865c0379ffc9be93b
Jangminez <111687963+Jangminez@users.noreply.github.com>
2024-08-04 21:37:23 +0900

    UI 수정
    Basic_UI & HP/MP UI 수정

--- Assets/02.Scripts/PlayerUIController.cs
+++ Assets/02.Scripts/PlayerUIController.cs
@@ -7,13 +7,11 @@ public class PlayerUIController : MonoBehaviour
 {
     private Player _player;
     [SerializeField] private Image _hpBar;
-    [SerializeField] private Text _hpText;
     [Space(10f)]
     [SerializeField] private Image _mpBar;
-    [SerializeField] private Text _mpText;
     [Space(10f)]
-    [SerializeField] private Image _expBar;
-    [SerializeField] private Text _expText;
+    [SerializeField] private Slider _expBar;
+
     [SerializeField] private Transform _RespawnUI;
 
     private void Start()
@@ -24,14 +22,13 @@ public class PlayerUIController : MonoBehaviour
     private void LateUpdate()
     {
         _hpBar.fillAmount = _player.Hp / _player.MaxHp;
-        _hpText.text = _player.Hp.ToString() + " / " + _player.MaxHp.ToString();
 
         _mpBar.fillAmount = _player.Mp / _player.MaxMp;
-        _mpText.text = _player.Mp.ToString() + " / " + _player.MaxMp.ToString();
 
-        _expBar.fillAmount = _player.Exp / _player.NextExp;
-        _expText.text = _player.Exp.ToString() + " / " + _player.NextExp.ToString();
+        _expBar.value = _player.Exp / _player.NextExp;
 
+
+        // 플레이어 사망 시 리스폰 UI 활성화
         if (_player._isDie && !_RespawnUI.gameObject.activeSelf)
         {
             _RespawnUI.gameObject.SetActive(true);
