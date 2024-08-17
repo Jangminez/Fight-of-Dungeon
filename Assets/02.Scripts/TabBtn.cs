a27d7551bd5c9ebda8f92f18bb569c52e169c892
Jangminez <111687963+Jangminez@users.noreply.github.com>
2024-08-17 16:53:34 +0900

    상점 탭 구현
    탭 구현 완

--- Assets/02.Scripts/TabBtn.cs
+++ Assets/02.Scripts/TabBtn.cs
@@ -6,12 +6,17 @@ using UnityEngine.UI;
 
 public class TabBtn : MonoBehaviour
 {
+    private enum Tabs { Weapon, Armor, Potion};
+    [SerializeField] Tabs _tab;
+
     private Button _myBtn;
     public Transform _myTab;
     public Transform[] _otherTabs;
 
     private ColorBlock _cb;
 
+    public Color32 _onColor;
+    public Color32 _offColor;
 
     private void Awake()
     {
@@ -20,12 +25,36 @@ public class TabBtn : MonoBehaviour
         _myBtn.onClick.AddListener(OpenTab);
 
         _cb = _myBtn.colors;
+
+        _onColor = new Color32(152, 178, 221, 255);
+        _offColor = new Color32(152, 152, 152, 255);
+
+        if (_tab == Tabs.Weapon)
+        {
+            _cb.normalColor = _onColor;
+            _myBtn.colors = _cb;
+            _myTab.gameObject.SetActive(true);
+        }
+
+    }
+
+    private void Update()
+    {
+        if (_myTab.gameObject.activeSelf)
+        {
+            _cb.normalColor = _onColor;
+            _myBtn.colors = _cb;
+        }
+
+        else
+        {
+            _cb.normalColor = _offColor;
+            _myBtn.colors = _cb;
+        }
     }
 
     void OpenTab()
     {
-        //_cb.normalColor = new Color32(152, 178, 221, 255);
-        _myBtn.colors = _cb;
         _myTab.gameObject.SetActive(true);
 
         foreach(Transform other in _otherTabs)
