883a9e963aacf4b75bc420ffe980eaa2e183c477
Jangminez <111687963+Jangminez@users.noreply.github.com>
2024-12-28 15:50:09 +0900

    Boss 공격 이펙트 추가

--- Assets/02.Scripts/EnemyScripts/Boss.cs
+++ Assets/02.Scripts/EnemyScripts/Boss.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class Boss : Enemy
+public class Boss : Enemy, IDamgeable
 {
     public bool _isPattern;
     public bool _isStand;
@@ -21,6 +21,8 @@ public class Boss : Enemy
     {
         if (!IsServer)
             return;
+
+        _initTransform = transform.position;
 
         _isPattern = false;
 
@@ -63,15 +65,15 @@ public class Boss : Enemy
 
                 rb.velocity = Vector2.zero;
 
-                if (
-                    _target != null
-                    && Vector2.Distance(_target.position, transform.position) < stat.chaseRange
-                    && !stat.isDie
-                    && !_isPattern
-                )
+                if (_target != null && Vector2.Distance(_target.position, transform.position) < stat.chaseRange && !stat.isDie)
                 {
                     timer = 0f;
                     state = States.Chase;
+                }
+
+                if (timer > 5f)
+                {
+                    state = States.Return;
                 }
             }
             else if (state == States.Chase)
@@ -142,48 +144,46 @@ public class Boss : Enemy
 
     public override IEnumerator EnemyAttack()
     {
-        while (_isAttack)
+        yield return new WaitForSeconds(0.1f);
+        // 공격시 방향 전환 및 애니메이션 실행
+        SetDirection();
+
+        r_Pattern = Random.Range(0f, 101f);
+
+        if (r_Pattern <= 50f)
         {
-            if (_isPattern){
-                yield return new WaitForSeconds(0.5f);
-                continue;
-            }
-                
-            // 공격시 방향 전환 및 애니메이션 실행
-            SetDirection();
-            r_Pattern = Random.Range(0f, 101f);
-
-            if (r_Pattern <= 50f)
-            {
-                // 기본 공격
-                _isPattern = true;
-                StartCoroutine(Boss_BasicAttack());
-
-            }
-
-            else if (50f < r_Pattern && r_Pattern <= 75f)
-            {
-                // 점프공격
-                _isPattern = true;
-                StartCoroutine(Boss_JumpAttack());
-            }
-
-            else
-            {
-                // 회전공격
-                _isPattern = true;
-                StartCoroutine(Boss_SpinAttack());
-            }
-
-            yield return new WaitForSeconds(1 / stat.attackSpeed);
-
-            if (state != States.Attack)
-            {
-                _isAttack = false;
-                _target = null;
-                yield break;
-            }
+            // 기본 공격
+            _isPattern = true;
+            StartCoroutine(Boss_BasicAttack());
         }
+
+        else if (50f < r_Pattern && r_Pattern <= 75f)
+        {
+            // 점프공격
+            _isPattern = true;
+            StartCoroutine(Boss_JumpAttack());
+        }
+
+        else
+        {
+            // 회전공격
+            _isPattern = true;
+            StartCoroutine(Boss_SpinAttack());
+        }
+
+        yield return new WaitForSeconds(1 / stat.attackSpeed);
+
+        if (state != States.Attack)
+        {
+            _isAttack = false;
+            _target = null;
+            yield break;
+        }
+    }
+
+    public void Hit(float damage)
+    {
+        TakeDamageServerRpc(damage);
     }
 
     public override IEnumerator HitEffect()
@@ -197,48 +197,40 @@ public class Boss : Enemy
             return;
 
 
-        if (state == States.Chase || state == States.Return)
+        if (state == States.Chase || state == States.Return || state == States.Attack)
         {
             anim.SetFloat("RunState", 1f);
         }
         else
         {
-            anim.SetFloat("RunState", 1f);
+            anim.SetFloat("RunState", 0f);
         }
     }
 
     private IEnumerator Boss_BasicAttack()
     {
-        anim.SetFloat("AttackState", 0f);
-        anim.SetBool("InAttack", true);
+        anim.SetTrigger("Attack");
 
-        yield return new WaitForSeconds(1 / stat.attackSpeed);
+        yield return new WaitForSeconds(1f);
 
-        anim.SetBool("InAttack", false);
-        _isPattern = false;
+        _isAttack = false;
     }
 
     private IEnumerator Boss_JumpAttack()
     {
-        anim.SetFloat("AttackState", 0.4f);
-        anim.SetBool("InAttack", true);
+        anim.SetTrigger("JumpAttack");
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(4f);
 
-        anim.SetFloat("AttackState", 0.7f);
-
-        anim.SetBool("InAttack", false);
-        _isPattern = false;
+        _isAttack = false;
     }
 
     private IEnumerator Boss_SpinAttack()
     {
-        anim.SetFloat("AttackState", 1f);
-        anim.SetBool("InAttack", true);
+        anim.SetTrigger("SpinAttack");
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4f);
 
-        anim.SetBool("InAttack", false);
-        _isPattern = false;
+        _isAttack = false;
     }
 }
