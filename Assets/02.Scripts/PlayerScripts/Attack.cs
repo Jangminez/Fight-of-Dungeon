cb27285b8cc28c7c27e5e3f29d18215341b8cfe7
Jangminez <111687963+Jangminez@users.noreply.github.com>
2025-01-09 03:47:07 +0900

    워록 스킬 제작완

--- Assets/02.Scripts/PlayerScripts/Attack.cs
+++ Assets/02.Scripts/PlayerScripts/Attack.cs
@@ -1,15 +1,17 @@
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class Attack : NetworkBehaviour
 {
+    public enum attackType { BasicAttack, Projectile, Repeated }
+    public attackType type;
     private Player player;
     float cri;
     bool isAttack = false;
-    [SerializeField] bool isProjectile;
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(!IsOwner) return;
+        if (!IsOwner) return;
 
         if (col.GetComponent<IDamgeable>() != null && !isAttack)
         {
@@ -18,7 +20,7 @@ public class Attack : NetworkBehaviour
             cri = Random.Range(1f, 101f); // 1 ~ 100 확률 지정
 
             // cri의 값이 크리티컬 범위 안에 존재한다면 크리티컬 공격
-            if(col.tag == "Player")
+            if (col.tag == "Player")
             {
                 player.AttackPlayerServerRpc(damage:
                 cri <= player.Critical ?
@@ -32,13 +34,13 @@ public class Attack : NetworkBehaviour
                 player.FinalAttack * 1.5f :
                 player.FinalAttack);
             }
-        }
 
-        if(isProjectile)
-        {
-            GetComponent<Animator>().SetTrigger("Hit");
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            Destroy(this.gameObject, 1f);
+            if (type == attackType.Projectile)
+            {
+                GetComponent<Animator>().SetTrigger("Hit");
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                Destroy(this.gameObject, 1f);
+            }
         }
     }
 }
---
+++ Assets/02.Scripts/PlayerScripts/WarlockSkills/WarlockSkill3.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class WarlockSkill3 : Skill
+{
+    public GameObject _attack;
+    private Player player;
+
+    [System.Serializable]
+    struct SkillInfo
+    {
+        public float damage; // 스킬 데미지
+        public float interval; // 공격 간격
+        public float coolDown; // 쿨타임
+        public float duration; // 지속시간
+        public Collider2D collider; // 콜라이더 
+        public List<Collider2D> montsterInRange; // 범위 안에 존재하는 몬스터 관리용
+    }
+    [SerializeField] SkillInfo _info;
+
+    void Awake()
+    {
+        // 스킬 정보 초기화
+        _info.damage = 0.7f;
+        _info.interval = 1.5f;
+        _info.coolDown = 50f;
+        _info.duration = 12f;
+        useMp = 20f;
+        _info.collider = GetComponent<Collider2D>();
+        _info.collider.enabled = false;
+        _info.montsterInRange = new List<Collider2D>();
+    }
+    public override IEnumerator SkillProcess()
+    {
+        if (!IsOwner) yield break;
+
+        // 쿨다운 시작
+        StartCoroutine(CoolDown(_info.coolDown));
+
+        // 지속시간이 끝나면 콜라이더 비활성화
+        _info.collider.enabled = true;
+        yield return new WaitForSeconds(_info.duration);
+        _info.collider.enabled = false;
+
+        // 애니메이션 중지
+        foreach (var anim in _anims)
+        {
+            anim.SetTrigger("End");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!IsOwner) return;
+
+        // 몬스터가 처음 스킬범위에 들어왔다면 추가 후 데미지 코루틴 시작
+        if (!_info.montsterInRange.Contains(other))
+        {
+            _info.montsterInRange.Add(other);
+            StartCoroutine(SkillDamage(other));
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!IsOwner) return;
+
+        // 범위에서 벗어나면 해당 몬스터 리스트에서 제거
+        _info.montsterInRange.Remove(other);
+    }
+
+    IEnumerator SkillDamage(Collider2D other)
+    {
+        if (!IsOwner) yield break;
+
+        player = GameManager.Instance.player;
+
+        var enemy = other.GetComponent<IDamgeable>();
+
+        cri = Random.Range(1, 101);
+
+        // 플레이어가 살아있고 몬스터가 범위 안에 있다면 데미지 부여
+        while (_info.montsterInRange.Contains(other) && !GameManager.Instance.player.Die)
+        {
+            if (enemy != null)
+            {
+                SpawnAttackServerRpc(other.transform.position + new Vector3(0f, 1f, 0f)); // 공격 이펙트 소환
+
+                for (int i = 0; i < 5; i++)
+                {
+                    cri = Random.Range(1f, 101f); // 1 ~ 100 확률 지정
+
+                    // cri의 값이 크리티컬 범위 안에 존재한다면 크리티컬 공격
+                    if (other.tag == "Player")
+                    {
+                        player.AttackPlayerServerRpc(damage:
+                        cri <= player.Critical ?
+                        player.FinalAttack * 1.5f :
+                        player.FinalAttack);
+                    }
+                    else
+                    {
+                        other.GetComponent<IDamgeable>().Hit(damage:
+                        cri <= player.Critical ?
+                        player.FinalAttack * 1.5f :
+                        player.FinalAttack);
+                    }
+
+                    yield return new WaitForSeconds(0.2f);
+                }
+            }
+            yield return new WaitForSeconds(_info.interval);
+        }
+
+        // 플레이어가 죽으면 애니메이션 중지
+        if (GameManager.Instance.player.Die)
+        {
+            foreach (var anim in _anims)
+            {
+                anim.SetTrigger("End");
+            }
+        }
+
+        StopCoroutine(SkillDamage(other));
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SpawnAttackServerRpc(Vector3 targetPosition, ServerRpcParams rpcParams = default)
+    {
+        GameObject attack = Instantiate(_attack, targetPosition, Quaternion.identity);
+        attack.GetComponent<NetworkObject>().SpawnWithOwnership(rpcParams.Receive.SenderClientId);
+
+        SetAttackClientRpc(attack.GetComponent<NetworkObject>().NetworkObjectId);
+        Destroy(attack, 0.9f);
+    }
+
+    [ClientRpc]
+    private void SetAttackClientRpc(ulong objectId)
+    {
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(objectId, out var attackObject))
+        {
+            if (attackObject.OwnerClientId == NetworkManager.Singleton.LocalClientId)
+            {
+                // 공격 생성 및 적용
+                Attack attack = attackObject.GetComponent<Attack>();
+                Destroy(attack, 0.9f);
+            }
+        }
+    }
+}
