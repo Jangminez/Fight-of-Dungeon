22c35b14b2790796ecb08d59840c7bd65589880a
Jangminez <111687963+Jangminez@users.noreply.github.com>
2025-02-11 13:26:03 +0900

    UI 팝업 및 사라지는 효과 추가

--- Assets/02.Scripts/PurchaseItem.cs
+++ Assets/02.Scripts/PurchaseItem.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -65,10 +63,10 @@ public class PurchaseItem : MonoBehaviour
                 break;
 
             case ItemType.Relic:
-                ScriptableRelic relic = RelicManager.Instance.GetRelic(itemValue);
-                relic.r_Count ++;
+                RelicManager.Instance.GetRelic(itemValue).r_Count ++;
                 break;
         }
+        purchaseInfo.SetActive(false);
     }
 }
 
