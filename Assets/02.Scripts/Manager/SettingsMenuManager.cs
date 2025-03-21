b0a4158863c90bc1e577da2f956588ef4924ea02
Jangminez <111687963+Jangminez@users.noreply.github.com>
2025-03-21 15:07:57 +0900

    설정값 PlayerPref로 저장

--- Assets/02.Scripts/Manager/SettingsMenuManager.cs
+++ Assets/02.Scripts/Manager/SettingsMenuManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -10,23 +8,42 @@ public class SettingsMenuManager : MonoBehaviour
     public Slider _musicVol, _sfxVol;
     public AudioMixer _mainAudioMixer;
 
+    void OnEnable()
+    {
+        _mainAudioMixer.SetFloat("MusicVol", PlayerPrefs.GetFloat("MusicVol"));
+        _mainAudioMixer.SetFloat("SfxVol", PlayerPrefs.GetFloat("SfxVol"));
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("QualitySet"));
+
+        _musicVol.value = PlayerPrefs.GetFloat("MusicVol");
+        _sfxVol.value = PlayerPrefs.GetFloat("SfxVol");
+        _graphicsDropdown.value = PlayerPrefs.GetInt("QualitySet");
+    }
 
     public void ChangeGraphicsQuality()
     {
         UISoundManager.Instance.PlayClickSound();
         QualitySettings.SetQualityLevel(_graphicsDropdown.value);
+
+        PlayerPrefs.SetInt("QualitySet", _graphicsDropdown.value);
+        PlayerPrefs.Save();
     }
 
     public void ChangeMusicVolume()
     {
         UISoundManager.Instance.PlayClickSound();
         _mainAudioMixer.SetFloat("MusicVol", _musicVol.value);
+
+        PlayerPrefs.SetFloat("MusicVol", _musicVol.value);
+        PlayerPrefs.Save();
     }
 
     public void ChangeSfxVolume()
     {
         UISoundManager.Instance.PlayClickSound();
         _mainAudioMixer.SetFloat("SfxVol", _sfxVol.value);
+
+        PlayerPrefs.SetFloat("SfxVol", _sfxVol.value);
+        PlayerPrefs.Save();
     }
 
     public void GotoMain()
